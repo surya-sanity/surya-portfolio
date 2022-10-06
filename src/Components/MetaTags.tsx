4a9654607096d0accb421318fe83483e9759bdc5
surya-sanity <surya.sanity@gmail.com>
2022-10-06 21:37:17 +0530

    vercel app url for meta tags changed

--- src/Components/MetaTags.tsx
+++ src/Components/MetaTags.tsx
@@ -4,11 +4,11 @@ const MetaTags = () => {
   console.log("url ->", process.env);
 
   return (<Helmet>
-    <meta property="og:url" content={`${process.env.PUBLIC_URL}/surya.jpeg`} />
+    <meta property="og:url" content={`${process.env.REACT_APP_VERCEL_URL}/surya.jpeg`} />
     <meta property="og:type" content="website" />
     <meta property="og:title" content="Surya T" />
     <meta property="og:description" content="I Design/Develop Stuff" />
-    <meta property="og:image" content={`${process.env.PUBLIC_URL}/surya.jpeg`} />
+    <meta property="og:image" content={`${process.env.REACT_APP_VERCEL_URL}/surya.jpeg`} />
     <meta name="theme-color" content="#000000" />
     <meta name="description" content="I Design/Develop Stuff" />
   </Helmet>)
