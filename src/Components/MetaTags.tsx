0ebe7effc254f3662b380acfb2746c76e7343b96
surya-sanity <surya.sanity@gmail.com>
2022-10-06 19:06:31 +0530

    logs for meta

--- src/Components/MetaTags.tsx
+++ src/Components/MetaTags.tsx
@@ -1,21 +1,15 @@
 import { Helmet } from "react-helmet";
 
-
 const MetaTags = () => {
+  console.log("url ->", process.env.PUBLIC_URL);
+
   return (<Helmet>
     <meta property="og:url" content={`${process.env.PUBLIC_URL}/surya.jpeg`} />
     <meta property="og:type" content="website" />
     <meta property="og:title" content="Surya T" />
     <meta property="og:description" content="I Design/Develop Stuff" />
     <meta property="og:image" content={`${process.env.PUBLIC_URL}/surya.jpeg`} />
-
-    <meta name="twitter:card" content="summary_large_image" />
-    <meta name="twitter:title" content="Surya T" />
-    <meta name="twitter:description" content="I Design/Develop Stuff" />
-    <meta name="twitter:image" content={`${process.env.PUBLIC_URL}/surya.jpeg`} />
-
     <meta name="theme-color" content="#000000" />
-    <meta property="og:image" content={`${process.env.PUBLIC_URL}/surya.jpeg`} />
     <meta name="description" content="I Design/Develop Stuff" />
   </Helmet>)
 }
